fd6a06d646aeb0ea39fba70a1160cb1ec83426a7
remberto-uaz <rsandoval@uaz.edu.mx>
2020-05-22 16:08:21 -0500

    next

--- MKL25Z128_Keypad_example/source/MKL25Z128_Keypad_example.c
+++ MKL25Z128_Keypad_example/source/MKL25Z128_Keypad_example.c
@@ -12,11 +12,15 @@
  *   list of conditions and the following disclaimer in the documentation and/or
  *   other materials provided with the distribution.
  *
+ *
+ *
+ *
  * o Neither the name of NXP Semiconductor, Inc. nor the names of its
  *   contributors may be used to endorse or promote products derived from this
  *   software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * THIS
+ * SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
